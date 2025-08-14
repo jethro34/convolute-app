2d5bce41e1b5d0401d39465ce4546714f61e8667
jethro34 <jh.triana@hotmail.com>
2025-08-13 23:18:03 -0400

    update run.py for Render deployment (dynamic port)

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,11 @@
 # convolute/backend/run.py
 
+import os
 from app import create_app
 from app.extensions import socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
